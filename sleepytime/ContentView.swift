d5b316a48209b9dbe41f0f873b1d178753d2b8bb
Vansh <patelvansh1023@gmail.com>
2026-08-23 23:39:49 -0400

    Initial Commit

---
+++ sleepytime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sleepytime
+//
+//  Created by Vansh Patel on 8/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sleepytime/sleepytimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleepytimeApp.swift
+//  sleepytime
+//
+//  Created by Vansh Patel on 8/23/26.
+//
+
+import SwiftUI
+
+@main
+struct sleepytimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
